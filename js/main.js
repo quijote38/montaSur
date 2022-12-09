073762108bd8bb1978c22fbf7491a8962b1dc994
Carlos Sanchez <carlosanchezv3@gmail.com>
2022-12-08 23:41:59 -0300

    resolviendo

--- js/main.js
+++ js/main.js
@@ -3,21 +3,19 @@ const agregandoCar = document.getElementById("imgCar");
 const containerCarrito = document.getElementById("container-carrito");
 const circuloCarrito = document.getElementById("circuloCarrito");
 
-swal({
-  title: "Bienvenido",
-  text: "Estás por comprar las mejores marcas para acampar",
-  button: "¡Vamos!"
-})
+// swal({
+//   title: "Bienvenido",
+//   text: "Estás por comprar las mejores marcas para acampar",
+//   button: "¡Vamos!"
+// })
 
 let carrito = JSON.parse(localStorage.getItem ("montaSur")) || [];
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetchData();
-});
+
 
 const fetchData = async () => {
   try {
-    const res = await fetch("/productos.json");
+    const res = await fetch('productos.json');
     const data = await res.json();
     productos(data);
   } catch (error) {
@@ -28,6 +26,11 @@ const fetchData = async () => {
     });
   }
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
 
 const productos = (data) => {
   data.forEach((producto) => {
@@ -76,4 +79,72 @@ const salvandoLocal = () =>{
   localStorage.setItem("montaSur", JSON.stringify (carrito))
 }
 
+
+const pintaCarrito = () => {
+  containerCarrito.innerHTML = "";
+  containerCarrito.style.display = "flex";
+  const pagCarHeader = document.createElement("div");
+  pagCarHeader.className = "headerPag";
+  pagCarHeader.innerHTML = `<h1 class= "headerPagTitulo">Cesta</h1>`;
+  containerCarrito.append(pagCarHeader);
+
+  const pagCarBoton = document.createElement("h2");
+  pagCarBoton.innerText = "x";
+  pagCarBoton.className = "botonPag";
+  pagCarHeader.append(pagCarBoton);
+
+  pagCarBoton.addEventListener("click", () => {
+    containerCarrito.style.display = "none";
+  });
+
+  carrito.forEach((producto) => {
+    let contenidoCar = document.createElement("div");
+    contenidoCar.className = "carritoContenidoPag";
+    contenidoCar.innerHTML = `
+  <img src= "${producto.img}">
+  <h3>${producto.nombre}</h3>
+  <p>${producto.precio} $</p>
+  <p> Cantidad: ${producto.cantidad} </p>
+  <p> Total: ${producto.cantidad * producto.precio} </p>`;
+
+    containerCarrito.append(contenidoCar);
+
+    let eliminando = document.createElement("span");
+    eliminando.innerText = "X";
+    eliminando.className = "borrarProducto";
+    contenidoCar.append(eliminando);
+    eliminando.addEventListener("click", eliminarProducto);
+  });
+
+  const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+
+  const compraTotal = document.createElement("div");
+  compraTotal.className = "totalPag";
+  compraTotal.innerHTML = `Tienes que pagar: ${total} `;
+  containerCarrito.append(compraTotal);
+};
+
+agregandoCar.addEventListener("click", pintaCarrito);
+
+const eliminarProducto = () => {
+  const foundId = carrito.find((element) => element.id);
+
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== foundId;
+  });
+
+  contenedorCirculo();
+  salvandoLocal ();
+  pintaCarrito();
+};
+
+const contenedorCirculo = () => {
+  circuloCarrito.style.display = "block";
+  const carritoLe= carrito.length;
+  localStorage.setItem ("carritoLe", JSON.stringify(carritoLe))
+  
+  circuloCarrito.innerText = JSON.parse(localStorage.getItem("carritoLe"));
+};
+
+contenedorCirculo ();
 
